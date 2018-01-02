589d15cb26124146b9275368998e3cf181423db6
Roger Soucy <roger.soucy@turn.com>
2018-01-02 16:25:29 -0500

    Added initialization functionality

--- src/gftk.js
+++ src/gftk.js
@@ -19,9 +19,14 @@ console.log('Glowforge Toolkit - by Elusive Concepts, LLC. (https://elusive-conc
 	 * @param {string} responseMessage
 	 */
 
-	/** @type {Boolean} - if the toolkit is loaded */
+	/** @type {boolean} - if the toolkit is loaded */
 	var _loaded = false;
 
+	/** @type {string} - Base URL of toolkit files **/
+	var _URL = './Toolkit/src/';
+
+	/** @type {object} - GF Toolkit Panel */
+	var _gftk = null;
 
 	/**
 	 * Toolkit Initialization
@@ -32,7 +37,22 @@ console.log('Glowforge Toolkit - by Elusive Concepts, LLC. (https://elusive-conc
 	{
 		if(_loaded == true) { return 0; }
 
-		console.log('Glowforge Toolkit: Loaded (Have fun \'forging!)');
+		GFTK.loader('https://www.amobee.com/version.txt', 'html', function()
+		{
+			GFTK.loader(_URL + 'gftk.css', 'css');
+
+			_gftk = document.createElement('div');
+			_gftk.id = 'gf-toolkit-wrapper';
+			_gftk.innerHTML = this.response;
+		
+			document.body.insertBefore(_gftk, document.getElementsByTagName('script')[0]);
+
+			_addEventListener('click', '.gftk-save', GFTK.saveSettings); 
+			
+			_loaded = true;
+
+			console.log('Glowforge Toolkit: Loaded (Have fun \'forging!)');
+		});
 	}
 
 	/**
@@ -44,6 +64,65 @@ console.log('Glowforge Toolkit - by Elusive Concepts, LLC. (https://elusive-conc
 	{
 		return _loaded;
 	}
+
+
+	/**
+	 * Open the GF Toolkit Panel
+	 */
+	GFTK.open = function()
+	{
+		_gftk.classList.add('open');
+	}
+
+
+	/**
+	 * Close the GF Toolkit Panel
+	 */
+	GFTK.close = function()
+	{
+		_gftk.classList.remove('open');
+	}
+
+	/**
+	 * Load a saved settings file
+	 */
+	GFTK.loadSettings = function()
+	{
+
+	}
+
+	/**
+	 * Save a settings file
+	 */
+	GFTK.saveSettings = function()
+	{
+		var settings = {};
+
+		settings.items = [];
+
+		settings.items.push({
+			name: 'Item 1',
+			order: 2,
+			process: 'Manual Engrave',
+			speed: 1000,
+			power: 50,
+			lpi: 225,
+			focus: 0.267
+		});
+
+		settings.items.push({
+			name: 'Item 2',
+			order: 1,
+			process: 'Proofgrade Score',
+			speed: 500,
+			power: 'FULL',
+			lpi: 225,
+			focus: 0.267
+		});
+
+		GFTK.export(settings, 'Test.settings');
+	}
+
 
 	/**
 	 * Load an external file and either inject or return the response
@@ -69,7 +148,24 @@ console.log('Glowforge Toolkit - by Elusive Concepts, LLC. (https://elusive-conc
 				el.href = url;
 				el.media = 'screen';
 
-				document.getElementsByTagName('head')[0].appendChild('el');
+				document.getElementsByTagName('head')[0].appendChild(el);
+				break;
+
+			case 'HTML':
+				var async = (typeof callback == 'function') ? true : false;
+				var request = new XMLHttpRequest();
+				    request.open('GET', url, async);
+				    request.send();
+
+				if(async)
+				{
+					request.onload = callback;
+					return;
+				}
+				else if(request.status === 200)
+				{
+					el = request.response;
+				}
 				break;
 
 			case 'IMAGE':
@@ -86,19 +182,23 @@ console.log('Glowforge Toolkit - by Elusive Concepts, LLC. (https://elusive-conc
 				break;
 
 			case 'JSON':
+				var async = (typeof callback == 'function') ? true : false;
 				var request = new XMLHttpRequest();
-				request.open('GET', url);
-				request.responseType = 'json';
+
+				request.open('GET', url, async);
+
+				if(async) { request.responseType = 'json'; }
+
 				request.send();
 
-				if(typeof callback == 'function')
+				if(async)
 				{
 					request.onload = callback;
 					return;
 				}
-				else
+				else if(request.status === 200)
 				{
-					request.onload = function() { el = request.response; }
+					el = JSON.parse(request.response);
 				}
 				break;
 
@@ -109,6 +209,46 @@ console.log('Glowforge Toolkit - by Elusive Concepts, LLC. (https://elusive-conc
 
 		return el;
 	}
+
+
+	/**
+	 * Save data as an external file.
+	 * 
+	 * @param {object|array} data - data to save
+	 * @param {string} name - filename
+	 */
+	GFTK.export = function(data, name)
+	{	
+		var a = document.createElement('a');
+
+console.log(data);
+console.log(JSON.stringify(data));
+
+		var f = new Blob([JSON.stringify(data)], {type: 'text/plain'});
+		    a.href = URL.createObjectURL(f);
+		    a.download = name;
+		    a.click();
+	}
+
+	
+	/**
+	 * Add a delegated event listener
+	 * 
+	 * @param {string} type - event type
+	 * @param {string} selector - delegation target selector
+	 * @param {function} callback - event callback
+	 */
+	var _addEventListener = function(type, selector, callback)
+	{
+		window.addEventListener(type, function(e)
+		{
+			if(e.target && e.target.matches(selector))
+			{
+				callback.call(e.target);
+			}
+		});
+	}
+
 
 	// Initialize the toolkit on first load
 	if(document.readyState === 'complete')
